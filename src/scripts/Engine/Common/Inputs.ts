0abf61e681dd71caf6cbad7ac511f4c27b78021c
experdot <experdot@foxmail.com>
2018-10-10 00:18:49 +0800

    Inputs: Prevent default touch event behaviour
    it may cause errors for bootstrap

--- src/scripts/Engine/Common/Inputs.ts
+++ src/scripts/Engine/Common/Inputs.ts
@@ -165,14 +165,17 @@ export class TouchInput extends InputBase {
         this.registEvent("tap", InputEvents.Tap);
         this.registEvent("touchStart", InputEvents.TouchStart, () => {
             event.stopPropagation();
+            event.preventDefault();
             inputs.touch.isTouching = true;
         });
         this.registEvent("touchEnd", InputEvents.TouchEnd, () => {
             event.stopPropagation();
+            event.preventDefault();
             inputs.touch.isTouching = false;
         });
         this.registEvent("touchmove", InputEvents.TouchMove, (event: TouchEvent) => {
             event.stopPropagation();
+            event.preventDefault();
             inputs.touch.position = new Vector2(event.changedTouches[0].clientX, event.changedTouches[0].clientY);
         });
     }
@@ -217,14 +220,17 @@ export class PointerInput extends InputBase {
         this.registEvent("tap", InputEvents.PointerClicked);
         this.registEvent("touchstart", InputEvents.PointerPressed, () => {
             event.stopPropagation();
+            event.preventDefault();
             inputs.pointer.isPressed = true;
         });
         this.registEvent("touchend", InputEvents.PointerReleased, () => {
             event.stopPropagation();
+            event.preventDefault();
             inputs.pointer.isPressed = false;
         });
         this.registEvent("touchmove", InputEvents.PointerMoved, (event: TouchEvent) => {
             event.stopPropagation();
+            event.preventDefault();
             inputs.pointer.isPressed = true;
             inputs.pointer.position = new Vector2(event.changedTouches[0].clientX, event.changedTouches[0].clientY);
         });
